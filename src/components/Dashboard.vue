36b8c5db2b4c50de883b093b3514de733e24301c
rubengp99 <rdgp99@gmail.com>
2020-07-21 14:52:55 -0400

    Mejorada la iconografía del sitio

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -10,13 +10,13 @@
                     </v-card-title>
 
                     <v-row justify="center" class="my-10 mx-5">
-                        <v-col cols="12" md="3" sm="12" v-for="(n,i) in 8" :key="n" class="mb-10">
-                            <v-card outlined elevation="0" width="100%" height="210" @click="e1 = 2">
+                        <v-col cols="12" md="3" sm="12" v-for="(n,i) in 8" :key="n" class="mb-6">
+                            <v-card outlined elevation="0" width="100%" height="175" @click="e1 = 2">
                                 <div class="tipo subtitle-2 ">{{imagenes[i].text}}</div>
                                 <v-row justify="center" class="pt-4">
-                                    <v-img contain width="100" height="100" :src="require('@/assets/'+imagenes[i].img)"></v-img>
+                                    <v-img class="bg" contain width="100" height="100" :src="require('@/assets/'+imagenes[i].img)"></v-img>
                                 </v-row>
-                                <v-divider class="mt-12"></v-divider>
+                                <v-divider class="mt-3"></v-divider>
                                 <div class="text-center font-weight-bold subtitle-1 mt-2">{{cambios[i]}}</div>
                             </v-card>
                         </v-col>
@@ -51,14 +51,14 @@ import Calculador from '@/components/Calculador';
             return {
                 e1:1,
                 imagenes:[
-                    {id:5,img:"bitcoin2.svg",text:"BTC"},
-                    {id:3,img:"euro.svg",text:"EUR"},
-                    {id:1,img:"dolar.svg",text:"USD"},
+                    {id:5,img:"bitcoin.png",text:"BTC"},
+                    {id:3,img:"euro.png",text:"EUR"},
+                    {id:1,img:"dollar.png",text:"USD"},
                     {id:4,img:"petro.png",text:"PTR"},
-                    {id:9,img:"eter.svg",text:"ETH"},
-                    {id:8,img:"litecoin.svg",text:"LTC"},
-                    {id:6,img:"dash.svg",text:"DASH"},
-                    {id:7,img:"dogecoin.svg",text:"DOGE"}
+                    {id:9,img:"ethereum.png",text:"ETH"},
+                    {id:8,img:"litecoin.png",text:"LTC"},
+                    {id:6,img:"dashcoin.png",text:"DASH"},
+                    {id:7,img:"dogecoin.png",text:"DOGE"}
                 ],
             }
         },
@@ -70,5 +70,10 @@ import Calculador from '@/components/Calculador';
         position:absolute;
         right:20px;
         top:10px;
+    }
+
+    .bg{
+        min-height:100px;
+        max-height:100px;
     }
 </style>
