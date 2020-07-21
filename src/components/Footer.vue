1f9d28704af242d159f37eeb8d2b90954b8a6608
rubengp99 <rdgp99@gmail.com>
2020-07-21 15:31:00 -0400

    Mejorada la disposicion de la calculadora (UI)

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
     <v-card height="50" color="#005598">
         <v-footer absolute class="font-weight-medium white--text" color="#005598">
             <v-col class="text-center" cols="12">
-                {{ new Date().getFullYear() }} — <strong><a href="https://somossistemas.com">Somos Sistemas C.A</a></strong>
+                {{ new Date().getFullYear() }} — <strong style="color:white;text-decoration:none;"><a href="https://somossistemas.com">Somos Sistemas C.A</a></strong>
             </v-col>
         </v-footer>
     </v-card>
